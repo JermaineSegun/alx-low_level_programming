a26626a390e851240858287685209e705ee954f0
JermaineSegun <olusegunakinlolu@gmail.com>
2022-06-15 14:18:08 -0700

    Write a function that prints a buffer.
    Prototype: void print_buffer(char *b, int size);
    The function must print the content of size bytes of the buffer pointed by b
    The output should print 10 bytes per line
    
    Each line starts with the position of the first byte of the line in hexadecimal (8 chars), starting with 0
    
    Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes at a time, separated by a space
    
    Each line shows the content of the buffer. If the byte is a printable character, print the letter, if not, print .
    
    Each line ends with a new line \n
    
    If size is 0 or less, the output should be a new line only \n
    
    You are allowed to use the standard library
    
    The output should look like the following example, and formatted exactly the same way:

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,45 +1,49 @@
-#include "holberton.h"
+
 #include <stdio.h>
 
 /**
- * print_buffer - prints string buffer
- * @b: the buffer
- * @size: size of buffer to print
- *
- * Return: void
+ * print_buffer - prints buffer of a char string
+ * @b: buffer to print
+ * @size: size of buffer
+ * Return: nothing
  */
 
 void print_buffer(char *b, int size)
 {
-int o = 0, i, j;
+int i, j;
 
 if (size <= 0)
-{
 printf("\n");
-return;
-}
-while (o < size)
-{
-j = size - o < 10 ? size - o : 10;
-printf("%08x: ", o);
-for (i = 0; i < 10; i++)
-{
-if (i < j)
-printf("%02x", *(b + o + i));
 else
-printf("  ");
-if (i % 2)
-printf(" ");
-}
-for (i = 0; i < j; i++)
 {
-int c = *(b + o + i);
-
-if (c < 32 || c > 132)
-c = '.';
-printf("%c", c);
+for (i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+j = 0;
+while (j < 10)
+{
+if (j % 2 == 0 && j != 0)
+printf(" ");
+if ((j + i) > size - 1)
+{
+printf("  ");
+}
+else
+printf("%.2x", b[i + j]);
+j++;
+}
+printf(" ");
+j = 0;
+while (j < 10)
+{
+if ((j + i) > size - 1)
+break;
+if (b[j + i] <= 31 || b[j + 1] >= '~')
+b[j + i] = '.';
+putchar(b[j + i]);
+j++;
 }
 printf("\n");
-o += 10;
+}
 }
 }
