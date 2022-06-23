54f33551b2f06c8a17810f67a6d51d13e22c3f83
JermaineSegun <olusegunakinlolu@gmail.com>
2022-06-22 23:12:06 -0700

    Create the static library libmy.a

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,35 +1,16 @@
-0-memset.o
+#include "main.h"
 
-0-strcat.o
+/**
+ * _isupper - return if c is uppercase
+ *
+ * @c: integer to examine
+ *
+ * Return: 1 if upper, 0 otherwise
+ */
 
-1-isdigit.o
-
-1-memcpy.o
-
-1-strncat.o
-
-100-atoi.o
-
-2-strchr.o
-
-2-strlen.o
-
-2-strncpy.o
-
-3-islower.o
-
-3-puts.o
-
-3-strcmp.o
-
-3-strspn.o
-
-4-isalpha.o
-
-4-strpbrk.o
-
-5-strstr.o
-
-6-abs.o
-
-9
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+return (1);
+return (0);
+}
