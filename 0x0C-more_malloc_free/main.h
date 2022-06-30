679d396ef0a61563dc4fd9e76f839a2f96e1d0e4
JermaineSegun <olusegunakinlolu@gmail.com>
2022-06-30 13:12:02 -0700

    changes to main and mul

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,10 +6,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int odl_size, unsigned int new_size);
-void _prntstr(char *s);
-long int numstrchk(char *s);
-void *_calloc_buffer(long int num, long int size);
-char *trimzero(char *s);
-int main(int ac, char **av);
+char *big_mult(char *s1, char *s2);
+int _putchar(char c);
+int _strlen(char *s);
 
 #endif
