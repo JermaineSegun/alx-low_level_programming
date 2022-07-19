26ffae3c47546f8cee24dfe8d053968fb1bb0000
JermaineSegun <olusegunakinlolu@gmail.com>
2022-07-19 02:37:58 -0700

    modify 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,79 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Listas enlazadas
- * @head: Dado desde main
- *
- * Return: i
+ * free_list2 - frees a list
+ * @head: head of the list
+ */
+
+void free_list2(listint_addr *head)
+{
+	listint_addr *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+
+}
+
+/**
+ * add_nodeaddr - adds new nodes to list
+ * @head: address new head
+ * @addr: address to store
+ * Return: head
+ */
+
+listint_addr *add_nodeaddr(listint_addr **head, const listint_t *addr)
+{
+	listint_addr *new_node;
+
+	new_node = malloc(sizeof(listint_addr));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+		free_list2(*head);
+		exit(98);
+	}
+	new_node->address = addr;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
+
+/**
+ * print_listint_safe - prints lists with loops
+ * @head: address pointer head
+ * Return: number of nodes
  */
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
-	const listint_t *temp, *node;
+	size_t n = 0;
+	listint_addr *addrs, *aux;
 
-	node = head;
-	while (node != NULL)
+	addrs = NULL;
+	while (head)
 	{
-		printf("[%p] %d\n", (void *)node, node->n);
-		temp = node;
-		node = node->next;
-		i++;
-		if (temp <= node)
+		aux = addrs;
+		while (addrs)
 		{
-			printf("-> [%p] %d\n", (void *)node, node->n);
-			break;
+			if (addrs->address == head)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list2(aux);
+				return (n);
+			}
+			addrs = addrs->next;
 		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		addrs = aux;
+		add_nodeaddr(&addrs, head);
+		head = head->next;
+		n++;
 	}
-	return (i);
+free_list2(addrs);
+return (n);
 }
